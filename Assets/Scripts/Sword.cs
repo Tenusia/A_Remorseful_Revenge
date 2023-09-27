dc179d3df38780416eda15b150f6109b3468bda4
Tenusia <110511640+Tenusia@users.noreply.github.com>
2023-09-27 22:50:53 +0200

    Fix sword animation to play on both sides

--- Assets/Scripts/Sword.cs
+++ Assets/Scripts/Sword.cs
@@ -6,8 +6,12 @@ public class Sword : MonoBehaviour
 {
     private PlayerControls playerControls;
     private Animator myAnimator;
+    private PlayerController playerController;
+    private ActiveWeapon activeWeapon;
     
     private void Awake() {
+        playerController = GetComponentInParent<PlayerController>();
+        activeWeapon = GetComponentInParent<ActiveWeapon>();
         myAnimator = GetComponent<Animator>();
         playerControls = new PlayerControls();
     }
@@ -21,7 +25,23 @@ public class Sword : MonoBehaviour
         playerControls.Combat.Attack.started += _ => Attack();
     }
 
+    private void Update() {
+        MouseFollowWithOffset();
+    }
+
     private void Attack() {
         myAnimator.SetTrigger("Attack");
+    }
+
+    private void MouseFollowWithOffset() {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(playerController.transform.position);
+
+        if (mousePos.x < playerScreenPoint.x) {
+            activeWeapon.transform.rotation = Quaternion.Euler(0, -180, 0);
+        }
+        else {
+            activeWeapon.transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
     }
 }
