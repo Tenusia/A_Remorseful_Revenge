bd5b823eacee276ed6581e4679baa4c1cfa5fd24
Tenusia <110511640+Tenusia@users.noreply.github.com>
2023-09-27 23:36:48 +0200

    Add weapon slash effect

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public bool FacingLeft { get {return facingLeft; } set { facingLeft = value; } }
+    
     [SerializeField] private float moveSpeed = 4f;
 
     private PlayerControls playerControls;
@@ -11,6 +13,8 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb;
     private Animator myAnimator;
     private SpriteRenderer mySpriteRender;
+
+    private bool facingLeft = false;
 
     private void Awake() {
         playerControls = new PlayerControls();
@@ -49,8 +53,10 @@ public class PlayerController : MonoBehaviour
 
         if(mousePos.x < playerScreenPoint.x) {
             mySpriteRender.flipX = true;
+            FacingLeft = true;
         } else {
             mySpriteRender.flipX = false;
+            FacingLeft = false;
         }
     }
 }
