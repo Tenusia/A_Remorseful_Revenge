c262d0cd0840fc5a3f1a229763f8637afc2aab53
Tenusia <110511640+Tenusia@users.noreply.github.com>
2023-09-28 21:10:17 +0200

    add Dash functionality

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     public static PlayerController Instance;
     
     [SerializeField] private float moveSpeed = 4f;
+    [SerializeField] private float dashSpeed = 4f;
+    [SerializeField] private TrailRenderer myTrailRenderer;
 
     private PlayerControls playerControls;
     private Vector2 movement;
@@ -16,6 +18,7 @@ public class PlayerController : MonoBehaviour
     private SpriteRenderer mySpriteRender;
 
     private bool facingLeft = false;
+    private bool isDashing = false;
 
     private void Awake() {
         Instance = this;
@@ -23,6 +26,10 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
         mySpriteRender = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start() {
+        playerControls.Combat.Dash.performed += _ => Dash();
     }
 
     private void OnEnable() {
@@ -60,5 +67,24 @@ public class PlayerController : MonoBehaviour
             mySpriteRender.flipX = false;
             FacingLeft = false;
         }
+    }
+
+    private void Dash() {
+        if (!isDashing) {
+            isDashing = true;
+            moveSpeed *= dashSpeed;
+            myTrailRenderer.emitting = true;
+            StartCoroutine(EndDashRoutine());
+        }
+    }
+
+    private IEnumerator EndDashRoutine() {
+        float dashTime = .2f;
+        float dashCD = .25f;
+        yield return new WaitForSeconds(dashTime);
+        moveSpeed /= dashSpeed;
+        myTrailRenderer.emitting = false;
+        yield return new WaitForSeconds(dashCD);
+        isDashing = false;
     }
 }
