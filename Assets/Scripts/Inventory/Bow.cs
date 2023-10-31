b22076145fc3dfde4e6a85fa8f61cae56918da45
Tenusia <110511640+Tenusia@users.noreply.github.com>
2023-10-31 22:24:45 +0100

    Add arrow projectile

--- Assets/Scripts/Inventory/Bow.cs
+++ Assets/Scripts/Inventory/Bow.cs
@@ -5,9 +5,20 @@ using UnityEngine;
 public class Bow : MonoBehaviour, IWeapon
 {
     [SerializeField] private WeaponInfo weaponInfo;
+    [SerializeField] private GameObject arrowPrefab;
+    [SerializeField] private Transform arrowSpawnPoint;
+
+    private Animator myAnimator;
+
+    readonly int FIRE_HASH = Animator.StringToHash("Fire");
+
+    private void Awake() {
+        myAnimator = GetComponent<Animator>();
+    }
     
     public void Attack() {
-        Debug.Log("BOW");
+        myAnimator.SetTrigger(FIRE_HASH);
+        GameObject newArrow = Instantiate(arrowPrefab, arrowSpawnPoint.position, ActiveWeapon.Instance.transform.rotation);
     }
 
     public WeaponInfo GetWeaponInfo() {
