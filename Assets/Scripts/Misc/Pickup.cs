7ecbefa4825335b8aa42daa36972075c45e97296
Tenusia <110511640+Tenusia@users.noreply.github.com>
2024-02-26 22:28:18 +0100

    Add health and Stamina pickups. Update coin bounce animations. Add custom cursor.

--- Assets/Scripts/Misc/Pickup.cs
+++ Assets/Scripts/Misc/Pickup.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Pickup : MonoBehaviour
 {
+    private enum PickupType
+    {
+        GoldCoin, 
+        StaminaGlobe,
+        HealthGlobe,
+    }
+
+    [SerializeField] private PickupType pickUpType;
     [SerializeField] private float pickUpDistance = 5f;
     [SerializeField] private float accelarationRate = .3f;
     [SerializeField] private float moveSpeed = 3f;
+    [SerializeField] private AnimationCurve animCurve;
+    [SerializeField] private float heightY = 1.5f;
+    [SerializeField] private float popDuration = 1f;
 
     private Vector3 moveDir;
     private Rigidbody2D rb;
 
     private void Awake() {
         rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start() {
+        StartCoroutine(AnimCurveSpawnRoutine());
     }
 
     private void Update() {
@@ -34,7 +50,47 @@ public class Pickup : MonoBehaviour
     
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.GetComponent<PlayerController>()) {
+            DetectPickupType();
             Destroy(gameObject);
+        }
+    }
+
+    private IEnumerator AnimCurveSpawnRoutine() {
+        Vector2 startpoint = transform.position;
+        float randomX = transform.position.x + Random.Range(-2f, 2f);
+        float randomY = transform.position.y + Random.Range(-1f, 1f);
+        
+        Vector2 endPoint = new Vector2(randomX, randomY);
+
+        float timePassed = 0f;
+
+        while (timePassed < popDuration) {
+            timePassed += Time.deltaTime;
+            float linearT = timePassed / popDuration;
+            float heightT = animCurve.Evaluate(linearT);
+            float height = Mathf.Lerp(0f, heightY, heightT);
+
+            transform.position = Vector2.Lerp(startpoint, endPoint, linearT) + new Vector2(0f, height);
+
+            yield return null;
+        }
+    }
+
+    private void DetectPickupType() {
+        switch (pickUpType)
+        {
+            case PickupType.GoldCoin:
+                // do gold coin stuff
+                Debug.Log("gold");
+                break;
+            case PickupType.StaminaGlobe:
+                // do stamina globe stuff
+                Debug.Log("stamina");
+                break;
+            case PickupType.HealthGlobe:
+                PlayerHealth.Instance.HealPlayer();
+                Debug.Log("heal");
+                break;
         }
     }
 }
